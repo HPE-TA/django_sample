60ccb262547056a094f7dcf57e8c1579fd41ad6c
root <root@app01.w1xdhosdvvfuxfi1trtmuref1g.xx.internal.cloudapp.net>
2018-06-13 11:58:21 +0000

    Change response

--- healthcheck/views.py
+++ healthcheck/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import os
 
 
 def healthcheck(request):
-    return HttpResponse("OK")
+    hostname = os.uname()[1]
+    return HttpResponse("Hello, from {}".format(hostname))
