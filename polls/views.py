3515c81294b4a4d0e64a096b09d1f28d99222df0
takahiro-kida <takahiro.kida@hpe.com>
2018-06-14 15:14:15 +0000

    Add output with hostname

--- polls/views.py
+++ polls/views.py
@@ -6,11 +6,15 @@ from django.urls import reverse
 from django.template import loader
 from .models import Question, Choice
 
+import os
+
 def index(request):
+    hostname = os.uname()[1]
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
+        'hostname': hostname,
     }
     #return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
@@ -20,14 +24,17 @@ def detail(request, question_id):
     #    question = Question.objects.get(pk=question_id)
     #except Question.DoesNotExist:
     #    raise Http404("Question does not exist")
+    hostname = os.uname()[1]
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question}) 
+    return render(request, 'polls/detail.html', {'question': question, 'hostname': hostname}) 
 
 def results(request, question_id):
+    hostname = os.uname()[1]
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    return render(request, 'polls/results.html', {'question': question, 'hostname': hostname})
 
 def vote(request, question_id):
+    hostname = os.uname()[1]
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -36,6 +43,7 @@ def vote(request, question_id):
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
+            'hostname': hostname,
         })
     else:
         selected_choice.votes += 1
